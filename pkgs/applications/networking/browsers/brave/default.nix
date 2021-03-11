b5eec621761fa8935c9dcf596d3c631cfddead78
Max Hille <mh@lambdasoup.com>
2021-03-11 15:12:58 +0100

    brave: support permanent cmd args

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -93,6 +93,7 @@ in
 stdenv.mkDerivation rec {
   pname = "brave";
   version = "1.21.74";
+  patches = [ ./user-flags.patch ];
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
